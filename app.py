db8a219f0fdbb671c42ade5e4a6c6cf752720fb8
Valdir Silva <valdir.souza_actdigital@prestador.globo>
2024-02-06 11:17:47 -0300

    feat: end login process

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, jsonify, request
 from database import db
 from models.user import User
-from flask_login import LoginManager
+from flask_login import LoginManager, login_user, current_user
 
 app = Flask(__name__)
-app.config['MY_SECRET_KEY'] = 'my_secret_key'
+app.config['SECRET_KEY'] = 'cd6bd383d4886c1345a9a3ed337407c16cddcce2'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 login_manager = LoginManager()
 
@@ -12,6 +12,7 @@ db.init_app(app)
 login_manager.init_app(app)
 
 # view login
+login_manager.login_view = 'login'
 
 @app.route('/')
 def hello_world() :
@@ -19,6 +20,10 @@ def hello_world() :
         'message': 'api is running'
     })
 
+@login_manager.user_loader
+def load_user(user_id):
+     return User.query.get(user_id)
+     
 @app.route('/login', methods=['POST'])
 def login():
     data = request.json
@@ -26,9 +31,9 @@ def login():
     password = data['password']
 
     if username and password:
-
         user = User.query.filter_by(username=username).first()
         if user and user.password == password:
+                login_user(user)
                 return jsonify({
                     'message': 'Authentication successfully'
                 })
