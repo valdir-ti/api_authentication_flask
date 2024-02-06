34da3ae666e8b66766c870fa379233c503b59b1b
Valdir Silva <valdir.souza_actdigital@prestador.globo>
2024-02-06 10:08:32 -0300

    feat: add first route and add database config

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
-print("API Authentication")
+from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['MY_SECRET_KEY'] = 'my_secret_key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+
+db = SQLAlchemy(app)
+
+@app.route('/')
+def hello_world():
+    return jsonify({
+        'message': 'api is running'
+    })
+
+if __name__ == '__main__':
+    app.run(debug=True)
