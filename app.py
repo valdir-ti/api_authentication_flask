0ffba567ed613fce43844345f7d8c13d60bcb44a
Valdir Silva <valdir.souza_actdigital@prestador.globo>
2024-02-06 11:06:57 -0300

    feat: get user from db

--- app.py
+++ app.py
@@ -1,17 +1,41 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from database import db
+from models.user import User
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['MY_SECRET_KEY'] = 'my_secret_key'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+login_manager = LoginManager()
 
 db.init_app(app)
+login_manager.init_app(app)
+
+# view login
 
 @app.route('/')
 def hello_world() :
     return jsonify({
         'message': 'api is running'
     })
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.json
+    username = data['username']
+    password = data['password']
+
+    if username and password:
+
+        user = User.query.filter_by(username=username).first()
+        if user and user.password == password:
+                return jsonify({
+                    'message': 'Authentication successfully'
+                })
+
+    return jsonify({
+        'message': 'Invalid credentials'
+    }), 400
 
 if __name__ == '__main__':
     app.run(debug=True)
